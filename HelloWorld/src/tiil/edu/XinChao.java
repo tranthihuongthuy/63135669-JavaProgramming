e66667ee142df553fccd3dd7499e23d57f49158c
thirr <tranthihuongthuy27@gmail.com>
2024-01-20 10:53:42 +0700

    Ví dụ đầu tiên
    Chỉ để demo

---
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 
+		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, và đang thử nghiệm quản lý mã nguồn với GIT");
+
+	}
+
+}
