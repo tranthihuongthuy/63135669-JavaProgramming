f48808f651a6aa32d9d35ab089c9ebf6b3049345
thirr <tranthihuongthuy27@gmail.com>
2024-01-20 11:14:10 +0700

    Lab1Bai1
    Bai 1

---
+++ Lab1/src/tiil/edu/Lab1Bai1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+import java.util.Scanner;
+public class Lab1Bai1 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Họ và tên: ");
+		String hoTen = scanner.nextLine();
+		System.out.print("Điểm TB: ");
+		double diemTB = scanner.nextDouble();
+		System.out.printf("%s %f điểm", hoTen, diemTB);
+	}
+}
