1fca81c39325704394b8c3f852d1680ff57c01d6
thirr <tranthihuongthuy27@gmail.com>
2024-01-20 10:56:54 +0700

    Cập nhật code
    sửa mới và thêm dòng

--- HelloWorld/src/tiil/edu/XinChao.java
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -13,8 +13,8 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// 
-		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, và đang thử nghiệm quản lý mã nguồn với GIT");
-
+		System.out.println("TÔI LẦN ĐẦU HỌC Java, và đang thử nghiệm quản lý mã nguồn với GIT");
+		System.out.print("Tôi thêm một dòng mới , và sửa lại chữ Java ở trên");
 	}
 
 }
